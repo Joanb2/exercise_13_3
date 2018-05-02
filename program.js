431c897c1a314a3ba56de996cd21ddf0f95c4035
Joanna Buchała <joanna.b2@op.pl>
2018-05-02 23:37:05 +0200

    another update

--- program.js
+++ program.js
@@ -1,12 +1,10 @@
 process.stdin.setEncoding('utf-8');
-
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
-    var instruction = input.toString().trim();
+    if(input !== null) {
+        var instruction = input.trim();
+    }
         switch(instruction) {
-            case 'input !== null':
-                process.stdout.write(input);
-                break;
             case '/exit':
                 process.stdout.write('Quitting app!\n');
                 process.exit();
@@ -14,6 +12,6 @@ process.stdin.on('readable', function() {
             default:
                 process.stderr.write('Wrong instruction!\n');
         };
-}); 
+});
 console.log(process.version);
 console.log(process.env);
