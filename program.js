8c4897678c5f592614675cb65627d3ef4368e67e
Joanna Buchała <joanna.b2@op.pl>
2018-05-04 20:52:04 +0200

    update

--- program.js
+++ program.js
@@ -1,17 +1,18 @@
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
-    if(input !== null) {
-        var instruction = input.trim();
+    if (!input) {
+        return;
     }
-        switch(instruction) {
-            case '/exit':
-                process.stdout.write('Quitting app!\n');
-                process.exit();
-                break;
-            default:
-                process.stderr.write('Wrong instruction!\n');
-        };
+    var instruction = input.toString().trim();
+    switch(instruction) {
+        case '/exit':
+            process.stdout.write('Quitting app!\n');
+          process.exit();
+            break;
+        default:
+            process.stderr.write('Wrong instruction!\n');
+    };
 });
 console.log(process.version);
 console.log(process.env);
